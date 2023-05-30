b20d8b27a230a7529d23868e01bd263060690d62
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-05-30 20:32:48 +0430

    Fixed linter errors

--- person.rb
+++ person.rb
@@ -9,11 +9,15 @@ class Person
     @parent_permission = parent_permission
   end
 
-  private def of_age?
+  private
+
+  def of_age?
     @age >= 18
   end
 
-  public def can_use_services?
+  public
+
+  def can_use_services?
     of_age? || @parent_permission
   end
 end
