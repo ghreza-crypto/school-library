37d194ddcf376ef90a2c755c450f7c09d572dcad
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-06-02 07:36:20 +0430

    Add relations in classroom and students

--- classroom.rb
+++ classroom.rb
@@ -6,5 +6,8 @@ class Classroom
       @label = label
       @students = []
     end
-  
+    def add_student(student)
+        @students << student
+        student.classroom = self
+      end
   end
